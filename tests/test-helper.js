454a127a5e0225e15a4961b08cd9c61d1db24227
Pankaj Kataria <mr.georgian4000@gmail.com>
2024-03-01 13:38:54 +0530

    Initial commit

---
+++ tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'ember-test-date-transform/app';
+import config from 'ember-test-date-transform/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
